ce640f7256c5d98e8bb6233ffe359e69d00429b6
Vadym Harmash <vadimvlad20032002@gmail.com>
2023-10-18 09:40:42 +0300

    fixed

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+        await mongoose.connect(process.env.DB_URL || 'mongodb+srv://vadym_harmash:03042003@cluster0.ufxegfn.mongodb.net/market?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
         console.log('Connected to DB')
     } catch (error) {
         console.error('DB connection error:', error)
